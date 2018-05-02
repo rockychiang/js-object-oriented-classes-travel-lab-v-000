4fd8272fd6627d4025d8edee80355a5d8bb94539
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-02 07:27:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class Driver {
   constructor(name, startDate) {
     this.name = name;
-    this.startDate = starDate;
+    this.startDate = new Date(starDate);
   }
   
   yearsExperienceFromBeginningOf(year){
