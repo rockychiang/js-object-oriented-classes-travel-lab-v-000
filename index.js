319582bee5227d3d52ca07af879d91050f27a05d
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-02 08:00:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ class Driver {
 
 class Route {
   constructor(beginningLocation, endingLocation){
-    
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
   }
   
   blocksTravelled(){
