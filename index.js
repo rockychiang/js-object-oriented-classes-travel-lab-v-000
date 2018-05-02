3e0d8a41052a8643b214232e0699ebe124d2fc88
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-02 08:06:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class Route {
   
   blocksTravelled(){
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-    const hTravelled = Math.abs(this.endingLocation.horizontal - this.beginningLocation.horizontal);
+    const hTravelled = Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - this.beginningLocation.horizontal);
     const vTravelled = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
     return hTravelled + vTravelled;
   }
