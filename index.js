9d825bd598185916044c714d0d3f0277ac387352
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-02 08:09:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+class Driver {
+  constructor(name, date) {
+    this.name = name;
+    this.startDate = new Date(date);
+  }
+  
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear()
+  }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  
+  blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    const hTravelled = Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal));
+    const vTravelled = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
+    return hTravelled + vTravelled;
+  }
+  
+  estimatedTime(peakHours){
+    if (peakHours) {
+      return this.blocksTravelled() / 2
+    } else {
+      return this.blocksTravelled() / 3 
+    }
+  }
+}
